da70390541cceeb9a766640ddacf430bacfd82a9
Lloyd Akresi <lloydakresi03@gmail.com>
2023-01-05 03:29:07 +0000

    complete

--- quicksort.js
+++ quicksort.js
@@ -2,17 +2,33 @@ function quicksort(arr) {
 
   // Check if the input is length 1 or less
     // If so, it's already sorted: return
+    if (arr.length <= 1) return arr;
 
   // Pick the first value as the pivot
+  let pivot = arr[0];
 
   // Orient the pivot so that...
       // every number smaller than the pivot is to the left
       // every number larger (or equal) than the pivot is to the right
+      let right = [];
+      let left = []
+      for(i = 0; i < arr.length; i++){
+
+        if (arr[i] > pivot){
+          right.push(arr[i]);
+        }else if(arr[i] < pivot){
+          left.push(arr[i]);
+        }
+
+      }
 
   // Recursively sort the left
+  left = quicksort(left);
   // Recursively sort the right
+  right = quicksort(right);
 
   // Return the left, pivot and right in sorted order
+  return [...left, pivot, ...right];
 
 }
 
